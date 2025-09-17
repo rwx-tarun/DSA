5cc59dae7c3c28f5fdd0cca3aa08dc56fa412db1
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2025-09-17 22:08:28 +0530

    Create 930. Binary Subarrays With Sum.cpp

---
+++ Sliding Window/930. Binary Subarrays With Sum.cpp
@@ -0,0 +1,29 @@
+// find subarray sum < goal  - func 
+
+// res = return func(nums, goal) - func(nums, goal - 1);
+
+O(2*n)
+SC - const 
+
+class Solution {
+    public:
+        int func(vector<int> &nums, int goal) {
+            if(goal < 0) return 0;
+            int l = 0, r = 0, sum = 0, cnt = 0;
+            while(r < nums.size()) {
+                sum += nums[r];
+                while(sum > goal) {
+                    sum -= nums[l];
+                    l++;
+                }
+                cnt += (r - l + 1);
+                r++;
+            }
+            return cnt;
+        }
+        
+        int numSubarraysWithSum(vector<int>& nums, int goal) {
+            return func(nums, goal) - func(nums, goal - 1);
+        }
+    };
+    
